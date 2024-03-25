a78409240d65118acffedb3ae083aead27a0d847
Adam Smith <>
2024-03-25 16:02:22 +0000

    deploy: add github page deployments

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -7,7 +7,7 @@ import { Forecast } from '../models/forecast.type';
 
 @Injectable()
 export class WeatherService {
-  static URL = 'http://api.openweathermap.org/data/2.5';
+  static URL = 'https://api.openweathermap.org/data/2.5';
   static APPID = '5a4b2d457ecbef9eb2a71e480b947604'; // Original
   // static APPID = '8f89eb41dd837a69826719d17d9f2ff3'; // Mine
   static ICON_URL =
